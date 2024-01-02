f7cb03068db13a2f75099e626967a6aa27608d59
heewon Choi <wonbb3313@gmail.com>
2024-01-02 22:56:50 +0900

    Feat: 관심사의 분리 - AppConfig

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,22 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepostory;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+
+    public MemberService memberService(){
+        return new MemberServiceImpl(new MemoryMemberRepostory());
+    }
+
+    public OrderService orderService(){
+        return new OrderServiceImpl(new MemoryMemberRepostory(), new FixDiscountPolicy());
+    }
+
+
+
+}
